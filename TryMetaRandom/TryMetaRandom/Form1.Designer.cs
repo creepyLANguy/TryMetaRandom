8fd822863ac38feecca37464d127ff3e82f5ec0c
Altamish Mahomed <gambit318@gmail.com>
2023-12-07 15:27:55 +0200

    Rough verification of concept. Downscaling introduces large error margin

---
+++ TryMetaRandom/TryMetaRandom/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TryMetaRandom
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.btn_previous = new System.Windows.Forms.Button();
+      this.btn_next = new System.Windows.Forms.Button();
+      this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // btn_previous
+      // 
+      this.btn_previous.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+      this.btn_previous.Location = new System.Drawing.Point(345, 915);
+      this.btn_previous.Name = "btn_previous";
+      this.btn_previous.Size = new System.Drawing.Size(159, 59);
+      this.btn_previous.TabIndex = 1;
+      this.btn_previous.Text = "<";
+      this.btn_previous.UseVisualStyleBackColor = true;
+      this.btn_previous.Click += new System.EventHandler(this.btn_previous_Click);
+      // 
+      // btn_next
+      // 
+      this.btn_next.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+      this.btn_next.Location = new System.Drawing.Point(510, 916);
+      this.btn_next.Name = "btn_next";
+      this.btn_next.Size = new System.Drawing.Size(165, 59);
+      this.btn_next.TabIndex = 2;
+      this.btn_next.Text = ">";
+      this.btn_next.UseVisualStyleBackColor = true;
+      this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(996, 898);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1022, 986);
+      this.Controls.Add(this.btn_next);
+      this.Controls.Add(this.btn_previous);
+      this.Controls.Add(this.pictureBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+    private System.Windows.Forms.Button btn_previous;
+    private System.Windows.Forms.Button btn_next;
+    private System.Windows.Forms.BindingSource bindingSource1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
---
+++ TryMetaRandom/TryMetaRandom/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace TryMetaRandom
+{
+  public partial class Form1 : Form
+  {
+    private static readonly Settings Config = new Settings(200, 200, 111, 5, 0.5f, false);
+    //private static Settings config = new Settings(200, 200, 111, 5, 0.5f, true);
+
+    private static readonly NearestNeighborPictureBox NearestNeighborPictureBox1 = new NearestNeighborPictureBox();
+    private static readonly List<Bitmap> Bitmaps = new List<Bitmap>();
+    private static int _currentIndex;
+    private static readonly Random Rand = new Random(Config.Seed);
+
+    public Form1()
+    {
+      InitializeComponent();
+
+      ReplacePictureBoxWithSpecialOne();
+
+      for (var i = 0; i < Config.Depth; ++i)
+      {
+        Bitmaps.Add(GetRandomBmp(i));
+      }
+
+      var metaImage = GetMetaImage();
+
+      Bitmaps.Add(metaImage);
+
+      _currentIndex = 0;
+
+      UpdatePictureBox();
+    }
+
+    private static Bitmap GetRandomBmp(int scaleFactor)
+    {
+      ++scaleFactor;
+      scaleFactor *= scaleFactor;
+
+      var bmp = new Bitmap(Config.Width / scaleFactor, Config.Height /scaleFactor);
+
+      for (var row = 0; row < bmp.Height; ++row)
+      {
+        for (var col = 0; col < bmp.Width; ++col)
+        {
+          if (Config.Greyscale)
+          {
+            var c = Rand.Next(256);
+            var colour = Color.FromArgb(c, c, c);
+            bmp.SetPixel(col, row, colour);
+          }
+          else
+          {
+            var colour = Color.FromArgb(Rand.Next(256), Rand.Next(256), Rand.Next(256));
+            bmp.SetPixel(col, row, colour);
+          }
+        }
+      }
+
+      return bmp;
+    }
+
+    private static Bitmap GetMetaImage()
+    {
+      var baseImage = new Bitmap(Bitmaps[0]);
+
+      using (var g = Graphics.FromImage(baseImage))
+      {
+        var colorMatrix = new ColorMatrix { Matrix33 = Config.MetaStrength };
+        var imageAttributes = new ImageAttributes();
+        imageAttributes.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+
+        for (var i = 1; i < Bitmaps.Count; ++i)
+        {
+          var overlayImage = Bitmaps[i];
+
+          g.DrawImage(
+            overlayImage,
+            new Rectangle(0, 0, baseImage.Width, baseImage.Height),
+            0,
+            0,
+            overlayImage.Width,
+            overlayImage.Height,
+            GraphicsUnit.Pixel,
+            imageAttributes);
+        }
+      }
+
+      return baseImage;
+    }
+
+    private void btn_previous_Click(object sender, EventArgs e)
+    {
+      _currentIndex = _currentIndex > 0 ? _currentIndex - 1 : Bitmaps.Count - 1;
+      UpdatePictureBox();
+    }
+
+    private void btn_next_Click(object sender, EventArgs e)
+    {
+      _currentIndex = _currentIndex < Bitmaps.Count - 1 ? _currentIndex + 1 : 0;
+      UpdatePictureBox();
+    }
+
+    private void UpdatePictureBox()
+    {
+      NearestNeighborPictureBox1.Image = Bitmaps[_currentIndex];
+      //btn_previous.Enabled = currentIndex > 0;
+      //btn_next.Enabled = currentIndex < bitmaps.Count - 1;
+    }
+
+    private void ReplacePictureBoxWithSpecialOne()
+    {
+      NearestNeighborPictureBox1.Location = pictureBox1.Location;
+      NearestNeighborPictureBox1.Size = pictureBox1.Size;
+      NearestNeighborPictureBox1.Anchor = pictureBox1.Anchor;
+      NearestNeighborPictureBox1.SizeMode = pictureBox1.SizeMode;
+      NearestNeighborPictureBox1.BackColor = Color.Black;
+      Controls.Add(NearestNeighborPictureBox1);
+      Controls.Remove(pictureBox1);
+    }
+
+    internal class NearestNeighborPictureBox : PictureBox
+    {
+      protected override void OnPaint(PaintEventArgs e)
+      {
+        if (Image == null)
+        {
+          return;
+        }
+        
+        e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+        var destRect = new Rectangle(0, 0, Width, Height);
+        e.Graphics.DrawImage(Image, destRect);
+      }
+    }
+
+    internal struct Settings
+    {
+      public int Width;
+      public int Height;
+      public int Seed;
+      public int Depth;
+      public float MetaStrength;
+      public bool Greyscale;
+
+      public Settings(int width, int height, int seed, int depth, float metaStrength, bool greyscale)
+      {
+        Width = width;
+        Height = height;
+        Seed = seed;
+        Depth = depth;
+        MetaStrength = metaStrength;
+        Greyscale = greyscale;
+      }
+    }
+
+  }
+}
